7cf3307835ca7dd9c9d36a1b0702fe030d29ed66
Maxon Gena <mgena@at-f5.21-school.ru>
2019-09-12 22:04:51 +0300

    12.09 1st part almost ready

--- ft_atol.c
+++ ft_atol.c
@@ -1,14 +1,50 @@
-#include "libft.h"
-#include <stdio.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgena <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/12 17:40:45 by mgena             #+#    #+#             */
+/*   Updated: 2019/09/12 21:57:17 by mgena            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-long ft_atol(const char *nptr)
+#include "libft.h"
+
+const char	*miss_space(const char *nptr)
 {
-	short flag;
-	long res;
-	
-	while (*nptr > 7 && *nptr < 14 && *nptr == 32)
+	while ((*nptr > 7 && *nptr < 14) || *nptr == 32)
 		nptr++;
+	return (nptr);
+}
+
+long		flag_ret(long res, const char *nptr, short flag)
+{
+	if (flag == 0)
+	{
+		if (res == 922337203685477580 && *nptr <= '7')
+			return (-((res * 10) + (*nptr - '0')));
+		else
+			return (-9223372036854775807 - 1);
+	}
+	else
+	{
+		if (res == 922337203685477580 && *nptr <= '7')
+			return (res * 10 + (*nptr - '0'));
+		else
+			return (9223372036854775807);
+	}
+}
+
+long		ft_atol(const char *nptr)
+{
+	short	flag;
+	long	res;
+
+	res = 0;
+	flag = 1;
+	nptr = miss_space(nptr);
 	if (*nptr == '-')
 		flag = 0;
 	else if (*nptr == '+')
@@ -17,19 +53,15 @@ long ft_atol(const char *nptr)
 		nptr++;
 	while (*nptr >= '0' && *nptr <= '9')
 	{
+		if (res >= 922337203685477580)
+		{
+			res = flag_ret(res, nptr, flag);
+			flag = 2;
+			break ;
+		}
 		res = res * 10 + (*nptr - '0');
 		nptr++;
 	}
+	res = (flag == 0) ? -res : res;
 	return (res);
-}
-	
-int main(void)	
-{
-	char *i = "214748364999999999997";
-	long a1, a2;
-
-	a1 = atol(i);
-	a2 = ft_atol(i);
-	printf("%ld\n", a1);
-	printf("%ld\n", a2);
 }
